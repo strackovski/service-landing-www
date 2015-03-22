36f82af4ab48c830368eac16184aebbabbcfb985
Vladimir Strackovski <vlado@nv3.org>
2015-03-22 16:56:54 +0100

    First commit.

---
+++ assets/scripts/main.js
@@ -0,0 +1,39 @@
+/*!
+ * Service-Business Landing Site Template
+ * Copyright 2015 Vladimir Stračkovski
+ * See https://github.com/strackovski/service-biz-www
+ */
+$(document).ready(function () {
+    $('.nav-btn').on('click', function (e) {
+        e.preventDefault();
+        $('body').toggleClass('expanded');
+    });
+
+    $(window).on('scroll', function() {
+        var win = $(window);
+        var scrollTop =  win.scrollTop();
+        if (scrollTop > 120) {
+            if ($('.main-menu').hasClass('fixed')) {
+            } else {
+                $('.main-menu').addClass('fixed');
+                $('.mobile-nav').addClass('fixed');
+            }
+        } else {
+            if ($('.main-menu').hasClass('fixed')) {
+                $('.main-menu').removeClass('fixed');
+                $('.mobile-nav').removeClass('fixed');
+            }
+        }
+    })
+
+    $(window).resize(function () {
+
+
+        if ($('body').hasClass('expanded')) {
+            var win_width = $(window).width();
+            if (win_width > 740) {
+                $('body').toggleClass('expanded');
+            }
+        }
+    });
+});
---
+++ readme.md
@@ -0,0 +1,20 @@
+Service-Business Landing Site Template
+=========
+A modern landing web site design for a small service type business site.
+
+Features
+----
+   * Attractive landing page with full-height hero for CTA content
+   * Cross-platform optimized and responsive design
+   * Easily adaptable to your needs
+   * Standards-compliant code (HTML5, CSS3, jQuery)
+   * PSD source available
+
+Assets
+----
+   * Photography assets for demo acquired from free sources
+   * Demo text content generated from http://www.lipsum.com/
+
+License
+----
+Designed and implemented by Vladimir Stračkovski, 2015. Please contact the author for further information about licensing options and customizations.
